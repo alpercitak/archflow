4a270ddda35eee6da3f203c5cdd716706af60dcf
Alper Çıtak <citak.alper@gmail.com>
2026-04-14 21:28:26 +0200

    flatten entrypoint

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,23 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { App } from './App';
+import Toast from '@/components/ui/toast';
+import Workspace from '@/features/workspace';
+import ContextMenu from '@/features/context-menu';
+import StatusBar from '@/features/status-bar';
+import TopBar from '@/features/top-bar';
+import './styles/global.css';
 
 const elem = document.getElementById('root')!;
 
 const app = (
   <StrictMode>
-    <App />
+    <div>
+      <TopBar />
+      <Workspace />
+      <StatusBar />
+      <Toast />
+      <ContextMenu />
+    </div>
   </StrictMode>
 );
 
